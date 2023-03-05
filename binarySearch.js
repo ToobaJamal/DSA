86f2d850b29cf9b8ea878c674038558cf3289262
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-03-05 22:36:49 +0500

    Create binarySearch.js

---
+++ binarySearch.js
@@ -0,0 +1,29 @@
+/*
+This function should accept an array and value and return the index at which the value exists or -1 if the value can not be found. 
+Do not use indexOf to solve this! This function should use iteration not recursion. Make sure that your algorithm runs in O(log(n)) and not O(n).
+*/
+
+function binarySearch(array, value) {
+    let midIndex 
+    let index
+    const nums = array.sort((a, b) => a - b)
+    midIndex = Math.floor(nums.length / 2)
+    if(array[midIndex] === value) {
+        index = midIndex
+    }
+    else if(array[midIndex] > value) {
+        for(let i=0; i<midIndex; i++) {
+            if(array[i] === value) {
+                index = i
+            }
+        }
+    }
+    else if (array[midIndex] < value) {
+        for(let i=midIndex; i<array.length; i++) {
+            if(array[i] === value) {
+                index = i
+            }
+        } 
+    }
+    return index
+}
